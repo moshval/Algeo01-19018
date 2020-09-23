83d82e7cabbdbc8382ca5fca54ab0c664cbda185
moshval <shevamohammad10@gmail.com>
2020-09-23 15:09:37 +0700

    Added tulismatriks + driver buat ngetes

---
+++ src/Driver.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import java.lang.*;
+
+public class Driver {
+    public static void main(String[] args){
+    // Cuma buat ngetes
+    Matriks M = new Matriks();
+    Scanner in = new Scanner(System.in);
+    M.bacaMatriks();
+    M.tulisMatriks();
+    }
+}
--- src/Matriks.java
+++ src/Matriks.java
@@ -1,13 +1,12 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.*;
 import java.util.*;
+import java.lang.*;
 
 // Konstruktor, menghandle baca matriks dan operasi dasar matriks
 
 public class Matriks{
-    double[][] M = new double[150][150]; // ukuran max 150 x 150
+    double[][] M = new double[150][150]; // inisialisasiukuran max 150 x 150
     int brs,kol;
     Matriks(){
         for (int i = 0; i < 150; i++) {
@@ -27,13 +26,25 @@ public class Matriks{
         this.brs = in.nextInt();
         System.out.print("Input jumlah kolom : ");
         this.kol = in.nextInt();
-        System.out.print("Input elemen matriks : ");
+        System.out.println("Input elemen matriks : ");
         for (i = 0; i < this.brs; i++) {
             for (j = 0; j < this.kol; j++) {
                 this.M[i][j] = in.nextDouble();
             }
         }
     }
+
+    public void tulisMatriks(){ //Menuliskan matriks ke layar
+        int i,j;
+        for ( i = 0; i < this.brs; i++) {
+            for ( j = 0; j < this.kol; j++) {
+                System.out.printf("%.4f ",this.M[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
 
 
 
