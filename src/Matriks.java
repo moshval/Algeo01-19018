3a2431e5ef236b2c4253fb441d6f9b8d3a72a835
moshval <shevamohammad10@gmail.com>
2020-09-23 13:52:08 +0700

    revisi dikit

--- src/Matriks.java
+++ src/Matriks.java
@@ -6,26 +6,35 @@ import java.util.*;
 
 // Konstruktor, menghandle baca matriks dan operasi dasar matriks
 
-class Matriks{
-    double[][] M;
+public class Matriks{
+    double[][] M = new double[150][150]; // ukuran max 150 x 150
     int brs,kol;
-    Matriks(int brs,int kol){
-        M = new double[brs][kol];
-        this.brs = brs;
-        this.kol = kol;
+    Matriks(){
+        for (int i = 0; i < 150; i++) {
+            for (int j = 0; j < 150; j++) {
+                this.M[i][j] = -999; //inisialisasi idxUndef 
+            }
+        }
+        this.brs = 0;
+        this.kol = 0;
 
     }
 
-    void bacaMatriks(){ //Input matriks dari masukan pengguna
+    public void bacaMatriks(){ //Input matriks dari masukan pengguna
         int i,j;
         Scanner in = new Scanner(System.in);
+        System.out.print("Input jumlah baris : ");
+        this.brs = in.nextInt();
+        System.out.print("Input jumlah kolom : ");
+        this.kol = in.nextInt();
+        System.out.print("Input elemen matriks : ");
         for (i = 0; i < this.brs; i++) {
             for (j = 0; j < this.kol; j++) {
-                System.out.print("Input elemen matriks M["+(i+1)+"]["+(j+1)+"] : ");
                 this.M[i][j] = in.nextDouble();
             }
         }
     }
+
 
 
 
