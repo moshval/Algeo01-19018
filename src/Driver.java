11f763891a74409b62f9792f429632aee3de496c
myahyaibrahim <yahyaibrahim01@gmail.com>
2020-09-25 10:55:34 +0700

    add echelon dan reducedEchelon

--- src/Driver.java
+++ src/Driver.java
@@ -1,22 +1,28 @@
-import java.io.*;
-import java.nio.*;
-import java.util.*;
-import java.lang.*;
+package algeo1;
+import java.util.Scanner;
 
 public class Driver {
-    public static void main(String[] args){
-    // Cuma buat ngetes
+	public static void main(String[] args) {
+		// Cuma buat ngetes
         double det;
         Matriks M = new Matriks();
         Matriks N = new Matriks();
+        Matriks O = new Matriks();
         Scanner in = new Scanner(System.in);
         M.bacaMatriks();
         M.tulisMatriks();
+        /*
         det = M.detKofaktor();
         System.out.printf("Determinan : %.2f",det);
         System.out.println();
         N = M.makeInverse();
         N.tulisMatriks();
+        */
+        N = M.echelon();
+        N.tulisMatriks();
         
-    }
+        O = M.reducedEchelon();
+        O.tulisMatriks();
+        
+	}
 }
